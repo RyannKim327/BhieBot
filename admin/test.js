3cbd72e1b54c1dc4e77cae273ffcd9e9f21e9f0a
RyannKim327 <rksesgundo123@gmail.com>
2024-10-27 10:39:19 +0800

    10-27-24 10:39 [Making AI more accurate]

--- admin/test.js
+++ admin/test.js
@@ -1,4 +1,5 @@
 module.exports = (api, event, regex) => {
+  api.setMessageReaction("😀", event.messageID, (e) => { }, true);
   api.sendMessage("Sent", event.threadID, (err, msg) => {
     if (err) return console.error(`ERR [Message]: ${JSON.stringify(err)}`);
 
--- auto/ai.js
+++ auto/ai.js
@@ -9,17 +9,27 @@ module.exports = async (api, event) => {
     json = JSON.parse(fs.readFileSync(name, "utf8"));
   }
   if (json[event.senderID] == undefined) {
+    const user = await api.getUserInfo(event.senderID);
     json[event.senderID] = [
       {
         role: "system",
         content:
           "Pretend to be the girlfriend of RyannKim327, and you named as Nyx. You're also a programmer focuses on web and android development.",
       },
+      {
+        role: "assistant",
+        content: `Hello ${user[event.senderID]["name"]} how are you. By the way my name is Nyx girlfriend bot of MPOP and also your friendly facebook bot. I focuses on programming related topics such as web development and android development.`,
+      },
     ];
+  }
+  let reply = "";
+  if (event.type === "message_reply") {
+    const sender = await api.getUserInfo(event.messageReply.senderID);
+    reply = `\nSaid by ${sender[event.messageReply.senderID]["name"]}\n> ${event.messageReply.body}`;
   }
   json[event.senderID].push({
     role: "user",
-    content: event.body,
+    content: `${event.body} ${reply}`,
   });
   gpt.chatCompletion(json[event.senderID]).then((res) => {
     api.sendMessage(
